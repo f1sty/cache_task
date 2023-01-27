eb67c3dd372023bfc2ed2f053853c26f3b97a7de
Yurii Skrynnykov <truef1s7@gmail.com>
2023-01-27 17:58:36 +0200

    Update supervisor tree
    Add `Cache`, `Cache.FunctionSupervisor` and `Cache.TasksSupervisor` into
    supervision tree.

--- lib/cache/application.ex
+++ lib/cache/application.ex
@@ -1,6 +1,4 @@
 defmodule Cache.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,11 @@ defmodule Cache.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Cache.Worker.start_link(arg)
-      # {Cache.Worker, arg}
+      Cache,
+      {Registry, [keys: :unique, name: Cache.FunctionRegistry]},
+      {Task.Supervisor, [name: Cache.TasksSupersisor]}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Cache.Supervisor]
     Supervisor.start_link(children, opts)
   end
